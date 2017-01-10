4aabf414f1cd7ff79e35122a9599a599d71643c4
adachi1206 <adachi@n-di.co.jp>
2017-01-10 11:45:42 +0900

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+const fs = require('fs'),
+gulp = require('gulp'),
+plumber = require("gulp-plumber"),
+sass = require('gulp-sass'),
+bulkSass = require('gulp-sass-bulk-import'),
+autoprefixer = require("gulp-autoprefixer"),
+sourcemaps = require("gulp-sourcemaps"),
+pug = require('gulp-pug'),
+browserSync = require('browser-sync').create();
+
+
+var running_tasks = [
+  'sass',
+  'pug',
+  'browser-sync'
+];
+
+gulp.task('browser-sync', function() {
+  browserSync.init({
+    server: {
+      baseDir: "static/"
+    }
+  });
+});
+
+
+gulp.task("pug", function() {
+    gulp.src(
+        ["pug/**/*.pug",'!' + "pug/**/_*.pug"]
+    )
+        .pipe(plumber())
+        .pipe(pug({pretty: true}))
+        .pipe(gulp.dest('static/'))
+    .pipe(browserSync.reload({stream:true}));
+});
+
+
+gulp.task('sass', function() {
+  gulp.src(["source/scss/**/*.scss","!source/scss/**/_*.scss"])
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(bulkSass())
+    .pipe(sass({outputStyle: "expanded", sourcemap:true}))
+    .pipe(autoprefixer('last 2 versions', 'ie 8', 'ie 9'))
+    .pipe(sourcemaps.write('./', {
+      includeContent: false
+    }))
+    .pipe(gulp.dest('static/css/'))
+    .pipe(browserSync.reload({stream:true}));
+});
+
+
+gulp.task('default', running_tasks, function() {
+  gulp.watch(['source/scss/**/*.scss'], ['sass']);
+  gulp.watch(['source/pug/**/*.pug'], ['pug']);
+});
